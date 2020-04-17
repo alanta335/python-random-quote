7034f1748b7c1855a90633c147f91bec8a7a1728
alanta335 <55522447+alanta335@users.noreply.github.com>
2020-04-18 01:24:57 +0500

    can add quotes now

--- get-quote.py
+++ get-quote.py
@@ -10,6 +10,19 @@ def m():
     rnd1=random.randint(0,last)
     q = quotes[rnd1].rstrip('\n')
     print(q)
+  print("do you what to add a quote then enter y less enter n ")
+  d = input()
+  if d=='y':
+    f = open("quotes.txt",'a')
+    print("enter your quote")
+    addq = str(input())
+    f.write(addq+'\n')
+    last+=1
+    f.close()
+  elif d=='n':
+    print("thank you")
+  else:
+    print("wrong input")
 
 if __name__== "__main__":
   m()
