c48ee2b2d666eaca29c6a1ac816682b8ca040b33
alanta335 <55522447+alanta335@users.noreply.github.com>
2020-04-18 00:36:44 +0500

    Randomly print more than one quotes

--- get-quote.py
+++ get-quote.py
@@ -4,9 +4,11 @@ def m():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last=13
-  rnd=random.randint(0,last)
-  print(quotes[rnd])
+  last=22
+  rnd2=random.randint(1,5)
+  for x in range(rnd2):
+    rnd1=random.randint(0,last)
+    print(quotes[rnd1])
 
 if __name__== "__main__":
   m()
